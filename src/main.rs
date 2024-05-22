439893e5eedd343b31c4225192735b9e8e17e80e
Obszczymucha <obszczymucha@gmail.com>
2024-05-22 10:24:04 +1000

    Refactor some more.

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,62 @@ use winapi::um::libloaderapi::GetModuleHandleW;
 use winapi::um::winuser::UnregisterHotKey;
 use winapi::um::winuser::{
     CallNextHookEx, DispatchMessageW, GetKeyState, GetMessageW, PostQuitMessage, RegisterHotKey,
-    SetWindowsHookExW, TranslateMessage, UnhookWindowsHookEx, KBDLLHOOKSTRUCT, MOD_ALT, MSG,
+    SetWindowsHookExW, TranslateMessage, UnhookWindowsHookEx, KBDLLHOOKSTRUCT, MSG,
     VK_MENU, WH_KEYBOARD_LL, WM_KEYDOWN, WM_SYSKEYDOWN,
 };
 
 const LEADER_KEY: u32 = b'A' as u32;
 const FOLLOWUP_KEY: u32 = b'X' as u32;
-const TIMEOUT_MS: u64 = 500;
+const TIMEOUT_MS: u64 = 650;
 const KEY_PRESSED_MASK: u16 = 0x8000;
+
+const MOD_CONTROL: u8 = 0x0002;
+const MOD_SHIFT: u8 = 0x0004;
+const MOD_WIN: u8 = 0x0008;
+const MOD_ALT: u8 = 0x0001;
+
+#[repr(u8)]
+enum Modifier {
+    // None = 0,
+    // Control = MOD_CONTROL,
+    // Shift = MOD_SHIFT,
+    // Win = MOD_WIN,
+    Alt = MOD_ALT,
+    // ControlShift = MOD_CONTROL | MOD_SHIFT,
+    // ControlWin = MOD_CONTROL | MOD_WIN,
+    // ControlAlt = MOD_CONTROL | MOD_ALT,
+    // ShiftWin = MOD_SHIFT | MOD_WIN,
+    // ShiftAlt = MOD_SHIFT | MOD_ALT,
+    // WinAlt = MOD_WIN | MOD_ALT,
+    // ControlShiftWin = MOD_CONTROL | MOD_SHIFT | MOD_WIN,
+    // ControlShiftAlt = MOD_CONTROL | MOD_SHIFT | MOD_ALT,
+    // ControlWinAlt = MOD_CONTROL | MOD_WIN | MOD_ALT,
+    // ShiftWinAlt = MOD_SHIFT | MOD_WIN | MOD_ALT,
+    // All = MOD_CONTROL | MOD_SHIFT | MOD_WIN | MOD_ALT,
+}
+
+struct LeaderKey {
+    code: u32,
+}
+
+struct KeySequence<'a> {
+    leader: LeaderKey,
+    next: Key<'a>,
+}
+
+enum Action {
+    Exit,
+}
+
+enum Key<'a> {
+    FollowUp { code: u32, next: &'a FollowUp<'a> },
+    Final { code: u32, action: Action },
+}
+
+enum FollowUp<'a> {
+    Next { code: u32, next: &'a FollowUp<'a> },
+    Final { code: u32, action: Action },
+}
 
 static mut HOOK: HHOOK = std::ptr::null_mut();
 static WAITING_FOR_NEXT_KEY: AtomicBool = AtomicBool::new(false);
@@ -72,7 +120,7 @@ struct HotkeyManager;
 impl HotkeyManager {
     fn new() -> Result<Self, &'static str> {
         unsafe {
-            if RegisterHotKey(std::ptr::null_mut(), 1, MOD_ALT as u32, LEADER_KEY) == 0 {
+            if RegisterHotKey(std::ptr::null_mut(), 1, Modifier::Alt as u32, LEADER_KEY) == 0 {
                 return Err("Failed to register hotkey.");
             }
         }
